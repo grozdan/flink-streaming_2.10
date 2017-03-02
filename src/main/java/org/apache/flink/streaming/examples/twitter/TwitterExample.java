536eb0989d7f986fda05812b06ca5b3ddc840eb9
Grozdan Madzarov <grozdan.madzarovamg@gmail.com>
2017-03-03 00:15:16 +0100

    RabbitMQ implementation

--- src/main/java/org/apache/flink/streaming/examples/twitter/TwitterExample.java
+++ src/main/java/org/apache/flink/streaming/examples/twitter/TwitterExample.java
@@ -17,17 +17,17 @@
 
 package org.apache.flink.streaming.examples.twitter;
 
-import java.util.Arrays;
-import java.util.Optional;
 import java.util.StringTokenizer;
-import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.rabbitmq.RMQSink;
+import org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 import org.apache.flink.streaming.examples.twitter.util.TwitterExampleData;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.flink.util.Collector;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -47,7 +47,6 @@ public class TwitterExample {
   // *************************************************************************
   // PROGRAM
   // *************************************************************************
-
 
   public static void main(String[] args) throws Exception {
 
@@ -74,74 +73,41 @@ public class TwitterExample {
         params.has(TwitterSource.TOKEN) &&
         params.has(TwitterSource.TOKEN_SECRET)
         ) {
+
       streamSource = env.addSource(new TwitterSource(params.getProperties()));
-      //DataStream<Tuple2<String, Integer>> tweets = streamSource
-      //    // selecting English tweets and splitting to (word, 1)
-      //    .flatMap(new SelectEnglishAndTokenizeFlatMap())
-      //    // group by words and sum their occurrences
-      //    .keyBy(0).sum(1);
 
-      DataStream<String> tweets = streamSource.flatMap(new FlatMapFunction<String, String>() {
-        @Override
-        public void flatMap(String value, Collector<String> out) throws Exception {
+      DataStream<String> dataStream = streamSource;
 
-          String[] words = value.split(" ");
-          Optional<String> opt = Arrays.stream(words).filter(word -> word.toLowerCase().equals("trump")).findAny();
-          if (opt.isPresent()) {
-            System.err.println(value);
-            for (String word : words) {
-              out.collect(word.toUpperCase());
-            }
-          }
-        }
-      }).filter(new FilterFunction<String>() {
-        @Override
-        public boolean filter(String value) throws Exception {
-          return value.toLowerCase().equals("trump");
-        }
-      });
+      dataStream.print();
 
-      if (params.has("output")) {
-        //tweets.writeAsText(params.get("output"));
-        //System.err.println(tweets);
-        System.out.println(tweets.printToErr());
-      } else {
-        System.out.println("Printing result to stdout. Use --output to specify output path.");
-        tweets.print();
-      }
+      final org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig connectionConfig =
+          new RMQConnectionConfig.Builder()
+              .setHost("localhost")
+              .setVirtualHost("/")
+              .setUserName("guest")
+              .setPassword("guest")
+              .setPort(5672)
+              .build();
 
-      // execute program
+      streamSource.addSink(new RMQSink<String>(
+          connectionConfig,            // config for the RabbitMQ connection
+          "gs-guide-websocket",                 // name of the RabbitMQ queue to send messages to
+          new SimpleStringSchema()));
+
       env.execute("Twitter Streaming Example");
     }
   }
 
-  // *************************************************************************
-  // USER FUNCTIONS
-  // *************************************************************************
-
-  /**
-   * Deserialize JSON from twitter source
-   *
-   * <p>
-   * Implements a string tokenizer that splits sentences into words as a
-   * user-defined FlatMapFunction. The function takes a line (String) and
-   * splits it into multiple pairs in the form of "(word,1)" ({@code Tuple2<String,
-   * Integer>}).
-   */
-  public static class SelectEnglishAndTokenizeFlatMap implements FlatMapFunction<String, Tuple2<String, Integer>> {
-    private static final long serialVersionUID = 1L;
+  public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
     private transient ObjectMapper jsonParser;
 
-    /**
-     * Select the language from the incoming JSON text
-     */
     @Override
-    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+    public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
       if (jsonParser == null) {
         jsonParser = new ObjectMapper();
       }
-      JsonNode jsonNode = jsonParser.readValue(value, JsonNode.class);
+      JsonNode jsonNode = jsonParser.readValue(sentence, JsonNode.class);
 
       boolean isEnglish = jsonNode.has("user") && jsonNode.get("user").has("lang") && jsonNode.get("user")
           .get("lang")
@@ -150,29 +116,14 @@ public class TwitterExample {
 
       boolean hasText = jsonNode.has("text");
       if (isEnglish && hasText) {
-        // message of tweet
-        String tweetText = jsonNode.get("text").asText();
-        System.err.println(tweetText);
 
         StringTokenizer tokenizer = new StringTokenizer(jsonNode.get("text").asText());
-
+        // System.err.println(jsonNode.get("text").asText());
         // split the message
         while (tokenizer.hasMoreTokens()) {
           String result = tokenizer.nextToken().replaceAll("\\s*", "").toLowerCase();
-
-          if (!result.equals("")) {
-            out.collect(new Tuple2<>(result, 1));
-          }
+          out.collect(new Tuple2<String, Integer>(result, 1));
         }
-      }
-    }
-  }
-
-  public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
-    @Override
-    public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
-      for (String word: sentence.split(" ")) {
-        out.collect(new Tuple2<String, Integer>(word, 1));
       }
     }
   }
